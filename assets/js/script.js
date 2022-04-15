110c01e73062353a948ec0e89cc8f336932532d7
Jayden Cowley <jcowley16@gmail.com>
2022-04-15 12:11:26 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,15 +71,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event, ui) {
     console.log(ui);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function(event, ui) {
     console.log(ui);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event) {
     console.log(event);
+    $(event.target).addClass("dropover-active")
   },
   out: function(event) {
     console.log(event);
+    $(event.target).removeClass("dropover-active")
   },
   update: function() {
     var tempArr = [];
@@ -126,9 +132,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active")
   }
 });
 
@@ -146,7 +154,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -279,4 +287,10 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+// timers
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
